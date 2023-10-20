83cd50928cacbff42691f7ddb088b857479f0ca5
miguelmazetto <33629994+miguelmazetto@users.noreply.github.com>
2023-10-20 19:53:30 -0300

    Version 0.0.2: First npm publish

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
+
 import { onMount } from 'svelte';
 
 /**
- * @type {import("@neutralinojs/lib/dist/neutralino").Neutralino | undefined}
+ * @type {import("@neutralinojs/lib/dist/neutralino.d.ts") | undefined}
  */
-var _Neutralino;
-export default _Neutralino;
 
-onMount(() => window.Neutralino && (_Neutralino = window.Neutralino))
+/** @type {(() => void)[]} */
+let cbs = [];
+let neut_loaded = false;
+
+export function onNeutralino(/** @type {() => void} */ cb) {
+    if(neut_loaded)
+        cb();
+    else
+        cbs.push(cb);
+}
+
+ /**
+  * @param {{ logErrors: boolean }} [options]
+  */
+export function initNeutralino(options)
+{
+    onMount(() => {
+
+        // Add neutralino.js to all files that requires it
+        var s = document.createElement('script');
+        s.setAttribute('src', '/neutralino.js')
+        s.onload = () => {
+            Neutralino.init()
+
+            if(options?.logErrors ?? true){
+                window.addEventListener('unhandledrejection', event => { 
+                    Neutralino.debug.log(`UnhandledRejection: ${event.reason}`,
+                        Neutralino.debug.LoggerType.ERROR)
+                }); 
+            }
+
+            neut_loaded = true;
+            cbs.forEach(cb => cb())
+        }
+        document.body.appendChild(s);
+
+    })
+}
